1348b167b8e0673cf20ce47e82820874a5caca8d
Marco Grier <mlgrier@gmail.com>
2018-12-24 17:49:06 -0600

    Update QuestionProvider.swift

--- EnhanceQuizStarter/QuestionProvider.swift
+++ EnhanceQuizStarter/QuestionProvider.swift
@@ -8,9 +8,14 @@
 
 struct TriviaProvider {
     let trivia: [[String : String]] = [
-        ["Question": "Only female koalas can whistle", "Answer": "False"],
-        ["Question": "Blue whales are technically whales", "Answer": "True"],
-        ["Question": "Camels are cannibalistic", "Answer": "False"],
-        ["Question": "All ducks are birds", "Answer": "True"]
+//        ["Question": "Only female koalas can whistle", "Answer": "False"],
+//        ["Question": "Blue whales are technically whales", "Answer": "True"],
+//        ["Question": "Camels are cannibalistic", "Answer": "False"],
+//        ["Question": "All ducks are birds", "Answer": "True"]
+        
+        ["Question": "What is 4 + 1", "Answer": "5"],
+        ["Question": "What is 1 + 1", "Answer": "1"],
+        ["Question": "What is 2 + 1", "Answer": "3"],
+        ["Question": "What is 4 + 4", "Answer": "8"]
     ]
 }
