856362a3772f1e930cd5907850c550c074cc1793
Marco Grier <mlgrier@gmail.com>
2018-12-24 18:15:19 -0600

    Add More Questions

--- EnhanceQuizStarter/QuestionProvider.swift
+++ EnhanceQuizStarter/QuestionProvider.swift
@@ -8,14 +8,17 @@
 
 struct TriviaProvider {
     let trivia: [[String : String]] = [
-//        ["Question": "Only female koalas can whistle", "Answer": "False"],
-//        ["Question": "Blue whales are technically whales", "Answer": "True"],
-//        ["Question": "Camels are cannibalistic", "Answer": "False"],
-//        ["Question": "All ducks are birds", "Answer": "True"]
-        
         ["Question": "What is 0 + 1", "Answer": "1"],
         ["Question": "What is 1 + 1", "Answer": "2"],
         ["Question": "What is 1 + 2", "Answer": "3"],
-        ["Question": "What is 1 + 3", "Answer": "4"]
+        ["Question": "What is 1 + 3", "Answer": "4"],
+        ["Question": "What is 9 - 8", "Answer": "1"],
+        ["Question": "What is 12 - 10", "Answer": "2"],
+        ["Question": "What is 13 - 10", "Answer": "3"],
+        ["Question": "What is 14 - 10", "Answer": "4"],
+        ["Question": "What is 7 - 6", "Answer": "1"],
+        ["Question": "What is 22 - 20", "Answer": "2"],
+        ["Question": "What is 33 - 30", "Answer": "3"],
+        ["Question": "What is 100 - 96", "Answer": "4"]
     ]
 }
